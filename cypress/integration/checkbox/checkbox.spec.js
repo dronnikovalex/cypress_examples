a79bd8524479041a84152caf1505bf031c726aee
Aleksey Dronnikov <hoock57@gmail.com>
2021-09-10 12:42:58 +0300

    checkboxes tutorial #done

---
+++ cypress/integration/checkbox/checkbox.spec.js
@@ -0,0 +1,35 @@
+/// <reference types="cypress" />
+
+describe('Handling checkboxes', () => {
+
+  it('Verify checking checkbox', () => {
+
+    cy.visit('http://www.webdriveruniversity.com/')
+    cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click()
+    cy.get('#checkboxes > :nth-child(1) > input').as('option-1')
+
+    cy.get('@option-1').check().should('be.checked')
+
+  })
+
+  it('Verify unchecking checkbox', () => {
+
+    cy.visit('http://www.webdriveruniversity.com/')
+    cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click()
+    cy.get('#checkboxes > :nth-child(5) > input').as('option-3')
+
+    cy.get('@option-3').uncheck().should('not.be.checked')
+
+  })
+
+  it('Check multiple checkbox', () => {
+
+    cy.visit('http://www.webdriveruniversity.com/')
+    cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click()
+    cy.get('input[type=checkbox]').as('checkboxes')
+
+    cy.get('@checkboxes').check(['option-1', 'option-2', 'option-3', 'option-4']).should('be.checked')
+
+  })
+
+})
