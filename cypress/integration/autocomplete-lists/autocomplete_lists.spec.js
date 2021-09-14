8b169c96c76f344ebf2c9bda0dbebb3c604dd1e9
Aleksey Dronnikov <hoock57@gmail.com>
2021-09-14 23:23:20 +0300

    autocomplete-list tests done

--- cypress/integration/autocomplete-lists/autocomplete_lists.spec.js
+++ cypress/integration/autocomplete-lists/autocomplete_lists.spec.js
@@ -7,7 +7,35 @@ describe('It testing browser navgiation', () => {
     cy.visit('http://www.webdriveruniversity.com/')
     cy.get('#autocomplete-textfield').invoke('removeAttr', 'target').click()
     cy.get('#myInput').type('A')
-    
+
+    cy.get('#myInputautocomplete-list > *').each($el => {
+
+      let item = $el.text()
+      let wantedItem = 'Avacado'
+
+      if (item == wantedItem) {
+        cy.wrap($el).click()
+        cy.get('#submit-button').click()
+        cy.url().should('include', wantedItem)
+      }
+    })
+      .then(() => {
+
+        cy.get('#myInput').type('G')
+
+        cy.get('#myInputautocomplete-list > *').each($el => {
+
+          let item = $el.text()
+          let wantedItem = 'Grapes'
+
+          if (item == wantedItem) {
+            cy.wrap($el).click()
+            cy.get('#submit-button').click()
+            cy.url().should('include', wantedItem)
+          }
+        })
+
+      })
   })
 
 })
