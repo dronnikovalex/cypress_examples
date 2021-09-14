7f189395ffc51e5a1db9327921d2301908dbbb8a
Aleksey Dronnikov <hoock57@gmail.com>
2021-09-14 22:09:26 +0300

    dropdown lists done

---
+++ cypress/integration/dropdown-lists/dropdown_lists.spec.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+describe('Handling drodpown lists', () => {
+
+  it('Selecting drodown items by using values attr', () => {
+    cy.visit('http://www.webdriveruniversity.com/')
+    cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click()
+    cy.get('#dropdowm-menu-1').select('c#').should('have.value', 'c#')
+    cy.get('#dropdowm-menu-2').select('maven').should('have.value', 'maven')
+    cy.get('#dropdowm-menu-3').select('javascript').should('have.value', 'javascript')
+  })
+
+  it('Selecting drodown items by using text values', () => {
+    cy.visit('http://www.webdriveruniversity.com/')
+    cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click()
+    cy.get('#dropdowm-menu-1').select('Python').should('have.value', 'python')
+    cy.get('#dropdowm-menu-2').select('JUnit').should('have.value', 'junit')
+    cy.get('#dropdowm-menu-3').select('CSS').should('have.value', 'css')
+  })
+
+  it('Dropdown lists challenge', () => {
+    cy.visit('http://www.webdriveruniversity.com/')
+    cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click()
+    cy.get('#dropdowm-menu-2').select('maven').should('have.value', 'maven')
+    cy.get('#dropdowm-menu-2').select('TestNG').contains('TestNG')
+  })
+
+})
