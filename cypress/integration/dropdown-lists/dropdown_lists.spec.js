ff066c099d5efe1650d20842de92aa6adbae9ee2
arcPC <arcpc@MacBook-Pro-arcPC.local>
2021-10-11 21:46:43 +0300

    correct/wrong ways to use constants in cypress

--- cypress/integration/dropdown-lists/dropdown_lists.spec.js
+++ cypress/integration/dropdown-lists/dropdown_lists.spec.js
@@ -3,7 +3,7 @@
 describe('Handling drodpown lists', () => {
 
   it('Selecting drodown items by using values attr', () => {
-    cy.visit('http://www.webdriveruniversity.com/')
+    cy.visit('/')
     cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click()
     cy.get('#dropdowm-menu-1').select('c#').should('have.value', 'c#')
     cy.get('#dropdowm-menu-2').select('maven').should('have.value', 'maven')
@@ -11,7 +11,7 @@ describe('Handling drodpown lists', () => {
   })
 
   it('Selecting drodown items by using text values', () => {
-    cy.visit('http://www.webdriveruniversity.com/')
+    cy.visit('/')
     cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click()
     cy.get('#dropdowm-menu-1').select('Python').should('have.value', 'python')
     cy.get('#dropdowm-menu-2').select('JUnit').should('have.value', 'junit')
@@ -19,7 +19,7 @@ describe('Handling drodpown lists', () => {
   })
 
   it('Dropdown lists challenge', () => {
-    cy.visit('http://www.webdriveruniversity.com/')
+    cy.visit('/')
     cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click()
     cy.get('#dropdowm-menu-2').select('maven').should('have.value', 'maven')
     cy.get('#dropdowm-menu-2').select('TestNG').contains('TestNG')
