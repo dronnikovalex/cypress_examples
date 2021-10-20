35a05650e3250888d8b6fa85364df7c27654b8de
ArcPc <dronnikov@arcsinus.ru>
2021-10-20 21:52:00 +0300

    debbuging lesson #close

--- cypress/support/PageObjects/automaiton-test-store/StoreHaircare_PO.js
+++ cypress/support/PageObjects/automaiton-test-store/StoreHaircare_PO.js
@@ -5,7 +5,12 @@ class AutoStoreHaircare {
   addHaircareProducts() {
     globalThis.products.productsNames.forEach(item => {
       cy.addMultipleItems(item)
+        // .then(() => {
+        //   debugger
+        // })
     })
+    //Different approach debug()
+    cy.get('.dropdown-toggle > .fa').click().debug()
   }
 
 }
