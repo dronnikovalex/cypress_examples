705f637d7fc4fcc9817abe2dc89192fb5735b1f9
Aleksey <hoock57@gmail.com>
2022-01-07 13:16:52 +0300

    crawl-spec #added

---
+++ cypress/integration/crawl-website/crawl.spec.js
@@ -0,0 +1,36 @@
+describe('It will crawl on a website', () => {
+
+  it('Should visit all links', () => {
+    const visited = new Set()
+    const toVisit = ['html-templates/crawl-website/index.html']
+
+    function visitNextUrl() {
+
+      if (!toVisit.length) {
+        return
+      }
+
+      cy.log(`to visit: ${toVisit.join(', ')} `)
+      const url = toVisit.shift()
+      visited.add(url)
+
+      cy.visit(url)
+      cy.get('a')
+        .should(Cypress._.noop)
+        .then($links => {
+          const links = $links
+            .toArray()
+            .map(link => link.pathname)
+            .filter(href => !href.startsWith('http') && !href.startsWith('//') && href!=='/')
+            .filter(href => !visited.has(href))
+            .filter(href => !toVisit.includes(href))
+          toVisit.push(...links)
+        })
+          .then(visitNextUrl)
+    }
+
+    visitNextUrl()
+
+  })
+
+})
