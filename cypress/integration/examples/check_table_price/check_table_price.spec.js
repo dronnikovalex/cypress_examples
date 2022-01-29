a2885e00536d2c1c2f5fb985de8a6c356d4233d1
Dronnikov Aleksey <hoock57@gmail.com>
2022-01-29 14:58:45 +0300

    Add index.html and base structure for check_price.spec

---
+++ cypress/integration/examples/check_table_price/check_table_price.spec.js
@@ -0,0 +1,9 @@
+describe('It tests price in a table ', () => {
+
+  it('Should check price', () => {
+
+    cy.visit('html-templates/simple_table/table.html')
+    cy.get('#s_table tbody tr').first().find()
+  })
+
+})
