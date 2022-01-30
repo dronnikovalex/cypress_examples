49a745e4fac48fd268bc1cf29948dfe03ded3e45
Aleksey <hoock57@gmail.com>
2022-01-30 18:49:43 +0300

    Add price checking test

--- cypress/integration/examples/check_table_price/check_table_price.spec.js
+++ cypress/integration/examples/check_table_price/check_table_price.spec.js
@@ -3,7 +3,18 @@ describe('It tests price in a table ', () => {
   it('Should check price', () => {
 
     cy.visit('html-templates/simple_table/table.html')
-    cy.get('#s_table tbody tr').first().find()
+    cy.get('#s_table tbody tr')
+      .each(row => {
+        cy.wrap(row)
+          .find('td')
+          .spread(($item, $price, $quantity, $total) => {
+            const name = $item.innerText
+            const price = +$price.innerText.replace('$', '')
+            const quantity = $quantity.innerText
+            const total = +$total.innerText.replace('$', '')
+            expect(total, `total ${name} price`).to.eql(price * quantity)
+          })
+      })
   })
 
 })
